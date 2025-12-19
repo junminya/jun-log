ba2f7b3a91fc8b307093b9a236036cdc47e97dd3
junminya <junminya@gmail.com>
2025-12-19 12:26:36 +0000

    fix: next.config.mjsにstandalone設定を追加

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // ▼ Docker化（Cloud Run）に必須の設定
+  output: 'standalone', 
+  
+  // ▼ 画像最適化の設定
   images: {
     remotePatterns: [
       {
